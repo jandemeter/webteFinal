046dc870383d65e74d3195ee0ba32d6504ca6657
laurakrajnak <laura.lk40@gmail.com>
2023-12-31 00:21:01 +0100

    L print, flags resize, confetti, design menu&help, borders

--- js/confetti.js
+++ js/confetti.js
@@ -4,15 +4,39 @@ const canvas = document.getElementById('confetti');
 const context = canvas.getContext("2d");
 const maxConfettis = 1000;
 const confettiParticles = [];
+const flagImages = [];
 
-const possibleColors = [
-    "#ffcc00",
-    "#ff6666",
-    "#ff99cc",
-    "#cc99ff",
-    "#66ccff",
-    "#99ffcc"
-];
+async function fetchData() {
+    try {
+        const response = await fetch("../data.json");
+        const data = await response.json();
+
+        // Assuming you have a specific region (e.g., Europe1) in mind
+        const regionFlags = data.flags.Europe1;
+
+        // Extract flag paths from the data
+        regionFlags.forEach(flag => {
+            const flagImage = new Image();
+            flagImage.src = flag.flag;
+            flagImages.push(flagImage);
+        });
+
+        // Shuffle flag images
+        shuffleArray(flagImages);
+
+        // Initialize confetti particles
+        for (let i = 0; i < maxConfettis; i++) {
+            confettiParticles.push(new ConfettiParticle());
+        }
+
+        // Other code remains the same...
+        canvas.width = screenWidth;
+        canvas.height = screenHeight;
+        drawConfetti();
+    } catch (error) {
+        console.error("Error fetching data:", error);
+    }
+}
 
 function getRandomInt(min, max) {
     return Math.floor(Math.random() * (max - min + 1) + min);
@@ -21,8 +45,8 @@ function getRandomInt(min, max) {
 function ConfettiParticle() {
     this.x = Math.random() * screenWidth;
     this.y = Math.random() * screenHeight;
-    this.radius = getRandomInt(5, 12);
-    this.color = possibleColors[getRandomInt(0, possibleColors.length - 1)];
+    this.radius = getRandomInt(15, 40); // Adjusted for slightly larger flags
+    this.flagImage = flagImages[getRandomInt(0, flagImages.length - 1)];
     this.velocityX = getRandomInt(-8, 8);
     this.velocityY = getRandomInt(-12, -6);
     this.rotation = getRandomInt(0, 360);
@@ -32,9 +56,7 @@ function ConfettiParticle() {
         context.save();
         context.translate(this.x, this.y);
         context.rotate(this.rotation * Math.PI / 180);
-        context.fillStyle = this.color;
-        context.arc(0, 0, this.radius, 0, Math.PI * 2);
-        context.fill();
+        context.drawImage(this.flagImage, -this.radius / 2, -this.radius / 2, this.radius, this.radius);
         context.restore();
         context.closePath();
     };
@@ -56,26 +78,12 @@ function drawConfetti() {
     }
 }
 
-function closeModal() {
-    const modalContainer = document.querySelector('.modal-container');
-    modalContainer.style.display = 'none';
+function shuffleArray(array) {
+    for (let i = array.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [array[i], array[j]] = [array[j], array[i]];
+    }
 }
 
-window.addEventListener(
-    "resize",
-    function () {
-        screenWidth = window.innerWidth;
-        screenHeight = window.innerHeight;
-        canvas.width = window.innerWidth;
-        canvas.height = window.innerHeight;
-    },
-    false
-);
-
-for (let i = 0; i < maxConfettis; i++) {
-    confettiParticles.push(new ConfettiParticle());
-}
-
-canvas.width = screenWidth;
-canvas.height = screenHeight;
-drawConfetti();
+// Call fetchData to initiate the confetti animation with larger flag images
+fetchData();
